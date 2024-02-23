1e59cad085c081d299999b4d59a1ab960b474283
vprovoto <vprovoto@cisco.com>
2024-02-23 20:59:06 +0200

    Add validation on crawl request.

--- src/main/java/com/experimental/webcrawler/controller/CrawlController.java
+++ src/main/java/com/experimental/webcrawler/controller/CrawlController.java
@@ -3,8 +3,10 @@ package com.experimental.webcrawler.controller;
 import com.experimental.webcrawler.dto.CrawlRequest;
 import com.experimental.webcrawler.dto.CrawlResponse;
 import com.experimental.webcrawler.service.CrawlerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class CrawlController {
     private final CrawlerService crawlerService;
     
     @PostMapping("/start")
-    public ResponseEntity<CrawlResponse> startCrawl(@RequestBody CrawlRequest crawlRequest) {
+    public ResponseEntity<CrawlResponse> startCrawl(@RequestBody @Valid CrawlRequest crawlRequest) {
         CrawlResponse crawlResponse = crawlerService.startCrawling(crawlRequest.getStartUrl(), crawlRequest.getThreadsCount());
         return ResponseEntity.ok(crawlResponse);
     }
