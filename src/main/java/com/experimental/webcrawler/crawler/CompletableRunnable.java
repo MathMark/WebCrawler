582406b237f8836fb0f53f59ecce337f24089845
vadimmartsun <vadimmartsun@gmail.com>
2024-02-25 12:58:06 +0200

    Add java doc for interfaces.

--- src/main/java/com/experimental/webcrawler/crawler/CompletableRunnable.java
+++ src/main/java/com/experimental/webcrawler/crawler/CompletableRunnable.java
@@ -1,5 +1,9 @@
 package com.experimental.webcrawler.crawler;
 
 public interface CompletableRunnable extends Runnable {
+    /**
+     * Sends a signal to a thread to stop, so thread has possibility
+     * to clear resources finish its work gracefully.
+     */
     void stop();
 }
