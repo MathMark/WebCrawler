1f1095206b18cf57d8b2b8adeef6687b98484d4f
vadimmartsun <vadimmartsun@gmail.com>
2024-10-15 18:55:52 +0300

    Refactoring

--- application-crawler/src/main/java/com/seo/service/CrawlerService.java
+++ application-crawler/src/main/java/com/seo/service/CrawlerService.java
@@ -28,8 +28,10 @@ import com.seo.repository.ProjectRepository;
 import com.seo.crawler.event.CrawlCompletedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -50,10 +52,10 @@ public class CrawlerService implements CrawlCompleteListener {
     private final ReportDocumentRepository reportDocumentRepository;
     private final PageRepository pageRepository;
     private final ExecutorService executorService = Executors.newFixedThreadPool(2);
-    
+
     private static final Pattern pattern = Pattern.compile("^(https?://[^/]+)");
     private final Map<String, CrawlTask> tasks = new HashMap<>();
-    
+
 
     public List<BasicCrawlStatus> getAllTasks() {
         return tasks.entrySet().stream().map(e -> BasicCrawlStatus.builder().taskId(e.getKey())
@@ -69,7 +71,7 @@ public class CrawlerService implements CrawlCompleteListener {
         CrawlTask crawlTask = objectProvider.getObject(data, crawlRequest.getThreadsCount());
         crawlTask.addListener(this);
         executorService.execute(crawlTask);
-    
+
         String taskId = UUID.randomUUID().toString();
         tasks.put(taskId, crawlTask);
         return CrawlResponse.builder()
@@ -126,87 +128,92 @@ public class CrawlerService implements CrawlCompleteListener {
         if (matcher.find()) {
             return matcher.group();
         }
-        return "";
+        return StringUtils.EMPTY;
     }
 
     @Override
     public void onCrawlCompete(CrawlCompletedEvent event) {
-        WebsiteProjectDocument websiteProjectDocument = mapToWebsiteProject(event.crawlData());
+        WebsiteProjectDocument websiteProjectDocument = Mapper.mapToWebsiteProject(event.crawlData());
         websiteProjectRepository.save(websiteProjectDocument);
-        List<WebPageDocument> pageEntities = mapToPageEntities(event.crawlData().getCrawledPages().values().stream().toList(), websiteProjectDocument.getId());
+        List<WebPageDocument> pageEntities = Mapper.mapToPageEntities(event.crawlData().getCrawledPages().values().stream().toList(), websiteProjectDocument.getId());
         pageRepository.saveAll(pageEntities);
-        BrokenPagesReportDocument brokenPagesReportDocument = this.mapToBrokenPageReport(event.crawlData().getBrokenWebPages(), websiteProjectDocument.getId());
+        BrokenPagesReportDocument brokenPagesReportDocument = Mapper.mapToBrokenPageReport(event.crawlData().getBrokenWebPages(), websiteProjectDocument.getId());
         reportDocumentRepository.save(brokenPagesReportDocument);
         log.info("Report for website {} has been successfully saved.", event.crawlData().getWebsite().domain());
     }
-    
-    public  BrokenPagesReportDocument mapToBrokenPageReport(List<BrokenWebPage> brokenWebPages, String websiteProjectId) {
-        List<BrokenPageEntity> brokenPageEntityList = brokenWebPages.stream()
-                .map(this::mapToBrokenPageEntity).collect(Collectors.toList());
-        BrokenPagesReportDocument brokenPagesReportDocument = new BrokenPagesReportDocument();
-        brokenPagesReportDocument.setId(UUID.randomUUID().toString());
-        brokenPagesReportDocument.setBrokenPages(brokenPageEntityList);
-        brokenPagesReportDocument.setWebsiteProjectId(websiteProjectId);
-        return brokenPagesReportDocument;
-    }
-    
-    private BrokenPageEntity mapToBrokenPageEntity(BrokenWebPage brokenWebPage) {
-        BrokenPageEntity brokenPageEntity = new BrokenPageEntity();
-        brokenPageEntity.setIncomingLinks(brokenWebPage.getWebPage().getIncomingLinks().stream().map(p -> {
-            IncomingLink incomingLink = new IncomingLink();
-            incomingLink.setUrl(p.getUrl());
-            incomingLink.setHrefText(p.getHrefText());
-            return incomingLink;
-        }).toList());
-        brokenPageEntity.setUrl(brokenWebPage.getWebPage().getUrl());
-        brokenPageEntity.setTextAttribute(null);
-        brokenPageEntity.setStatusCode(brokenWebPage.getStatusCode());
-        return brokenPageEntity;
-    }
-    
-    private WebsiteProjectDocument mapToWebsiteProject(CrawlData crawlData) {
-        WebsiteProjectDocument websiteProjectDocument = new WebsiteProjectDocument();
-        Website website = crawlData.getWebsite();
-        websiteProjectDocument.setId(crawlData.getId());
-        websiteProjectDocument.setInitialUrl(website.startUrl());
-        websiteProjectDocument.setDomain(website.domain());
-        websiteProjectDocument.setName(website.projectName());
-        websiteProjectDocument.setCrawledPages(crawlData.getCrawledPages().size());
-        return websiteProjectDocument;
-    }
 
-    private List<WebPageDocument> mapToPageEntities(List<WebPage> pages, String websiteProjectId) {
-        return pages.stream().map(p -> mapToPageEntity(p, websiteProjectId)).collect(Collectors.toList());
-    }
-    
-    private  WebPageDocument mapToPageEntity(WebPage webPage, String webProjectId) {
-        List<IncomingLinkDocument> incomingLinkDocuments = webPage.getIncomingLinks().stream().map(this::mapToLinkEntity).collect(Collectors.toList());
-        return WebPageDocument.builder().url(webPage.getUrl())
-                .content(mapToContentEntity(webPage.getContent()))
-                .incomingLinkDocuments(incomingLinkDocuments)
-                .webProjectId(webProjectId).build();
-    }
-    
-    private IncomingLinkDocument mapToLinkEntity(Link link) {
-        IncomingLinkDocument incomingLinkDocument = new IncomingLinkDocument();
-        incomingLinkDocument.setUrl(link.getUrl());
-        incomingLinkDocument.setHrefText(link.getHrefText());
-        return incomingLinkDocument;
-    }
-    
-    private ContentEntity mapToContentEntity(Content content) {
-        if (content != null) {
-            return ContentEntity.builder()
-                    .h1(content.getH1())
-                    .h2(content.getH2())
-                    .h3(content.getH3())
-                    .h4(content.getH4())
-                    .h5(content.getH5())
-                    .h6(content.getH6())
-                    .metaDescription(content.getMetaDescription())
-                    .metaRobots(content.getMetaRobots())
-                    .title(content.getTitle()).build();
+    private static class Mapper {
+        
+        private Mapper() {}
+        
+        private static BrokenPagesReportDocument mapToBrokenPageReport(List<BrokenWebPage> brokenWebPages, String websiteProjectId) {
+            List<BrokenPageEntity> brokenPageEntityList = brokenWebPages.stream()
+                    .map(Mapper::mapToBrokenPageEntity).collect(Collectors.toList());
+            BrokenPagesReportDocument brokenPagesReportDocument = new BrokenPagesReportDocument();
+            brokenPagesReportDocument.setId(UUID.randomUUID().toString());
+            brokenPagesReportDocument.setBrokenPages(brokenPageEntityList);
+            brokenPagesReportDocument.setWebsiteProjectId(websiteProjectId);
+            return brokenPagesReportDocument;
         }
-        return null;
+
+        private static BrokenPageEntity mapToBrokenPageEntity(BrokenWebPage brokenWebPage) {
+            BrokenPageEntity brokenPageEntity = new BrokenPageEntity();
+            brokenPageEntity.setIncomingLinks(brokenWebPage.getWebPage().getIncomingLinks().stream().map(p -> {
+                IncomingLink incomingLink = new IncomingLink();
+                incomingLink.setUrl(p.getUrl());
+                incomingLink.setHrefText(p.getHrefText());
+                return incomingLink;
+            }).toList());
+            brokenPageEntity.setUrl(brokenWebPage.getWebPage().getUrl());
+            brokenPageEntity.setTextAttribute(null);
+            brokenPageEntity.setStatusCode(brokenWebPage.getStatusCode());
+            return brokenPageEntity;
+        }
+
+        private static WebsiteProjectDocument mapToWebsiteProject(CrawlData crawlData) {
+            WebsiteProjectDocument websiteProjectDocument = new WebsiteProjectDocument();
+            Website website = crawlData.getWebsite();
+            websiteProjectDocument.setId(crawlData.getId());
+            websiteProjectDocument.setInitialUrl(website.startUrl());
+            websiteProjectDocument.setDomain(website.domain());
+            websiteProjectDocument.setName(website.projectName());
+            websiteProjectDocument.setCrawledPages(crawlData.getCrawledPages().size());
+            return websiteProjectDocument;
+        }
+
+        private static List<WebPageDocument> mapToPageEntities(List<WebPage> pages, String websiteProjectId) {
+            return pages.stream().map(p -> mapToPageEntity(p, websiteProjectId)).collect(Collectors.toList());
+        }
+
+        private static WebPageDocument mapToPageEntity(WebPage webPage, String webProjectId) {
+            List<IncomingLinkDocument> incomingLinkDocuments = webPage.getIncomingLinks().stream().map(Mapper::mapToLinkEntity).collect(Collectors.toList());
+            return WebPageDocument.builder().url(webPage.getUrl())
+                    .content(mapToContentEntity(webPage.getContent()))
+                    .incomingLinkDocuments(incomingLinkDocuments)
+                    .webProjectId(webProjectId).build();
+        }
+
+        private static IncomingLinkDocument mapToLinkEntity(Link link) {
+            IncomingLinkDocument incomingLinkDocument = new IncomingLinkDocument();
+            incomingLinkDocument.setUrl(link.getUrl());
+            incomingLinkDocument.setHrefText(link.getHrefText());
+            return incomingLinkDocument;
+        }
+
+        private static ContentEntity mapToContentEntity(Content content) {
+            if (content != null) {
+                return ContentEntity.builder()
+                        .h1(content.getH1())
+                        .h2(content.getH2())
+                        .h3(content.getH3())
+                        .h4(content.getH4())
+                        .h5(content.getH5())
+                        .h6(content.getH6())
+                        .metaDescription(content.getMetaDescription())
+                        .metaRobots(content.getMetaRobots())
+                        .title(content.getTitle()).build();
+            }
+            return null;
+        }
     }
 }
